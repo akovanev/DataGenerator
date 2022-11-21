1f387bb29712d33c588ce88f751c6978956b3327
akovanev <akovanev@gmail.com>
2022-11-21 12:38:05 +0100

    Add collection support

--- Akov.DataGenerator/DG.cs
+++ Akov.DataGenerator/DG.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Akov.DataGenerator.Common;
 using Akov.DataGenerator.Generators;
 using Akov.DataGenerator.Mappers;
@@ -33,15 +35,27 @@ namespace Akov.DataGenerator
 
         public string GenerateJson<T>(DgProfileBase profile)
             => GenerateJson(new TypeDataProcessor<T>(profile, _generatorFactory));
+        
+        public string GenerateJsonCollection<T>(DgProfileBase profile, int count)
+            => GenerateJsonCollection(new TypeDataProcessor<T>(profile, _generatorFactory), count);
 
         public string GenerateJson(DataScheme scheme)
             => GenerateJson(new DataProcessor(scheme, _generatorFactory));
 
+        public string GenerateJsonCollection(DataScheme scheme, int count)
+            => GenerateJsonCollection(new DataProcessor(scheme, _generatorFactory), count);
+
         public T GenerateObject<T>(DataScheme scheme)
             => JsonConvert.DeserializeObject<T>(GenerateJson(scheme));
+        
+        public IEnumerable<T> GenerateObjectCollection<T>(DataScheme scheme, int count)
+            => JsonConvert.DeserializeObject<IEnumerable<T>>(GenerateJsonCollection(scheme, count));
 
         public T GenerateObject<T>(DgProfileBase profile)
             => JsonConvert.DeserializeObject<T>(GenerateJson<T>(profile));
+        
+        public IEnumerable<T> GenerateObjectCollection<T>(DgProfileBase profile, int count)
+            => JsonConvert.DeserializeObject<IEnumerable<T>>(GenerateJsonCollection<T>(profile, count));
 
         public void SaveToFile(string filename, string json)
             => _ioHelper.Value.SaveData(filename, json);
@@ -50,6 +64,18 @@ namespace Akov.DataGenerator
         {
             var data = dataProcessor.CreateData();
             return JsonValueObjectSerializer.Serialize(data);
+        }
+        
+        private static string GenerateJsonCollection(IDataProcessor dataProcessor, int count)
+        {
+            var dataList = Enumerable.Range(1, count)
+                .Select(x =>
+                {
+                    var data = dataProcessor.CreateData();
+                    return JsonValueObjectSerializer.Serialize(data);
+                });
+            
+            return $"[{string.Join(",", dataList)}]";
         }
     }
 }
