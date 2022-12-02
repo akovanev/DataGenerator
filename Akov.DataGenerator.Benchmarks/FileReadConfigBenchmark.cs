b6ed9e3865270e0494a02ae274a0824fa8253c7c
akovanev <akovanev@gmail.com>
2022-12-02 10:08:11 +0100

    Add IntegrationTestsBenchmark

---
+++ Akov.DataGenerator.Benchmarks/FileReadConfigBenchmark.cs
@@ -0,0 +1,26 @@
+﻿using Akov.DataGenerator.Common;
+using Akov.DataGenerator.Demo.StudentsSample.Responses;
+using Akov.DataGenerator.Demo.StudentsSampleTests.Tests.Generators;
+using Akov.DataGenerator.Demo.StudentsSampleTests.Tests.Profiles;
+using BenchmarkDotNet.Attributes;
+
+namespace Akov.DataGenerator.Benchmarks;
+
+[MemoryDiagnoser]
+public class FileReadConfigBenchmark
+{
+    private readonly DG _generator1 = new (new StudentGeneratorFactory());
+    private readonly DG _generator2 = new (new StudentGeneratorFactory(), fileReadConfig: new FileReadConfig { UseCache = true});
+
+    [Benchmark]
+    public StudentCollection GenerateObject()
+    {
+        return _generator1.GenerateObject<StudentCollection>(new StudentsTestProfile());
+    }
+    
+    [Benchmark]
+    public StudentCollection GenerateObject_FilesCached()
+    {
+        return _generator2.GenerateObject<StudentCollection>(new StudentsTestProfile());
+    }
+}
--- Akov.DataGenerator.Benchmarks/Program.cs
+++ Akov.DataGenerator.Benchmarks/Program.cs
@@ -1,28 +1,8 @@
-﻿using Akov.DataGenerator;
-using Akov.DataGenerator.Common;
-using Akov.DataGenerator.Demo.StudentsSample.Responses;
-using Akov.DataGenerator.Demo.StudentsSampleTests.Tests.Generators;
-using Akov.DataGenerator.Demo.StudentsSampleTests.Tests.Profiles;
-using BenchmarkDotNet.Attributes;
+﻿using Akov.DataGenerator.Benchmarks;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
 
-BenchmarkRunner.Run<Benchmark>();
-
-[MemoryDiagnoser]
-public class Benchmark
-{
-    private readonly DG _generator1 = new (new StudentGeneratorFactory());
-    private readonly DG _generator2 = new (new StudentGeneratorFactory(), fileReadConfig: new FileReadConfig { UseCache = true});
-
-    [Benchmark]
-    public StudentCollection GenerateObject()
-    {
-        return _generator1.GenerateObject<StudentCollection>(new StudentsTestProfile());
-    }
-    
-    [Benchmark]
-    public StudentCollection GenerateObject_FilesCached()
-    {
-        return _generator2.GenerateObject<StudentCollection>(new StudentsTestProfile());
-    }
-}
+BenchmarkRunner.Run<IntegrationTestsBenchmark>(
+    DefaultConfig.Instance
+        .AddJob(Job.Default.WithBaseline(true)));
