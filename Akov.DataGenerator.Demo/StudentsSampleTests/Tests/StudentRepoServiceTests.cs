73f9e9b3ee9a768e2086e5d73c81812d7589aab1
akovanev <akovanev@gmail.com>
2022-12-05 01:08:52 +0100

    Add reset last run results

--- Akov.DataGenerator.Demo/StudentsSampleTests/Tests/StudentRepoServiceTests.cs
+++ Akov.DataGenerator.Demo/StudentsSampleTests/Tests/StudentRepoServiceTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,7 +8,9 @@ using Akov.DataGenerator.Demo.StudentsSample.Services;
 using Akov.DataGenerator.Demo.StudentsSampleTests.Tests.DgModels;
 using Akov.DataGenerator.Demo.StudentsSampleTests.Tests.Generators;
 using Akov.DataGenerator.Demo.StudentsSampleTests.Tests.Profiles;
+using Akov.DataGenerator.Extensions;
 using Akov.DataGenerator.Mappers;
+using Akov.DataGenerator.RunBehaviors;
 using Moq;
 using Xunit;
 
@@ -16,18 +19,22 @@ namespace Akov.DataGenerator.Demo.StudentsSampleTests.Tests;
 /// <summary>
 /// The test class for the StudentRepoService.
 /// </summary>
-public class StudentRepoServiceTests
+public class StudentRepoServiceTests : IDisposable
 {
     private readonly DG _dg;
     private readonly Mock<IStudentRepository> _studentRepositoryMock;
+    private readonly Mock<IStudentRepository> _studentRepositoryRepeatRunMock;
+    private readonly IRunBehavior _runBehavior = new StoreToFileRunBehavior();
 
     public StudentRepoServiceTests()
     {
         _dg = new DG(
             new StudentGeneratorFactory(),
             new DataSchemeMapperConfig { UseCamelCase = true },
-            new FileReadConfig { UseCache = true });
+            new FileReadConfig { UseCache = true },
+            _runBehavior);
         _studentRepositoryMock = new Mock<IStudentRepository>();
+        _studentRepositoryRepeatRunMock = new Mock<IStudentRepository>();
     }
     
     [Theory]
@@ -51,7 +58,34 @@ public class StudentRepoServiceTests
         // Assert
         Assert.NotNull(students);
         Assert.True(students.All(s => s.IsValid));
+        
+        // Arrange
+        var dataRepeatRun = type is GenerationType.UseAttributes
+            ? _dg.GenerateObjectCollection<Student>(_dg.GetFromType<DgStudent>(), 100, true)
+            : _dg.GenerateObjectCollection<Student>(new StudentsTestProfile(), 100, true);
+        
+        _studentRepositoryRepeatRunMock.Setup(x => x.GetAll())
+            .ReturnsAsync(dataRepeatRun);
+
+        studentService = new StudentRepoService(_studentRepositoryMock.Object);
+        
+        // Act
+        var studentsRepeatRun = (await studentService.GetAll()).ToList();
+        
+        // Assert
+        Assert.NotNull(studentsRepeatRun);
+        Assert.True(studentsRepeatRun.All(s => s.IsValid));
+        
+        Assert.Equal(students.First().FullName, studentsRepeatRun.First().FullName);
+        Assert.Equal(students.Last().FullName, studentsRepeatRun.Last().FullName);
 
         return students;
+    }
+
+    public void Dispose()
+    {
+        // Clear
+        _runBehavior.ClearResult($"{nameof(Student)}_collection");
+        _runBehavior.ClearResult($"{nameof(DgStudent)}_collection");
     }
 }
