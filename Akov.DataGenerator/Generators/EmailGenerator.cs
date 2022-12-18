1fd4b8a0a9bbd44e911e676d121fcb9d3e7712ae
akovanev <akovanev@gmail.com>
2022-12-18 22:56:28 +0100

    Extract GetSplitSizeOrString to extension

--- Akov.DataGenerator/Generators/EmailGenerator.cs
+++ Akov.DataGenerator/Generators/EmailGenerator.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using Akov.DataGenerator.Common;
 using Akov.DataGenerator.Constants;
+using Akov.DataGenerator.Extensions;
 using Akov.DataGenerator.Models;
 
 namespace Akov.DataGenerator.Generators;
@@ -60,43 +61,8 @@ public class EmailGenerator : GeneratorBase
     private string GetValueFromSet(string resource)
     {
         var values = ResourceReader.ReadEmbeddedResource(resource)!;
-        var (count, _) = GetSplitSizeOrString(values, ",");
+        var (count, _) = values.GetSplitSizeOrString(",");
         int random = new Random().Next(count);
-        return GetSplitSizeOrString(values, ",", random).Item2;
-    }
-    
-    private static (int, string) GetSplitSizeOrString(string source, string separator, int substring = -1)
-    {
-        int count = 0;
-        int prev = 0;
-        int i;
-
-        for (i = 0; i < source.Length; i++)
-        {
-            if (source[i] != separator[0]) continue;
-
-            bool isMatch = true;
-
-            for (int j = 1; j < separator.Length; j++)
-            {
-                if (source[i + j] == separator[j]) continue;
-                
-                isMatch = false;
-                break;
-            }
-
-            if (!isMatch) continue;
-
-            if (count == substring)
-                return (count + 1, source.Substring(prev, i - prev));
-
-            if (i + separator.Length == source.Length) break;
-            
-            prev = i + separator.Length;
-            i = prev - 1;
-            count++;
-        }
-
-        return (count + 1, source[prev..]);
+        return values.GetSplitSizeOrString(",", random).Item2;
     }
 }
